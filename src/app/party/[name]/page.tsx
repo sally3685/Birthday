7c3383327efbecc47849d1a26f30d782db39d3ee
Sally Ismail <he84tt@gmail.com>
2025-04-28 13:04:11 +0300

    third

--- src/app/party/[name]/page.tsx
+++ src/app/party/[name]/page.tsx
@@ -5,7 +5,9 @@ import styles from '@/app/styles/Fire.module.css';
 import Fire from '@/app/components/Fire';
 
 export async function generateStaticParams() {
-  const names = await fetch(`${process.env.PATH}`).then((res) => res.json());
+  const names = await fetch(`${process.env.NEXT_PUBLIC_URL}`).then((res) =>
+    res.json()
+  );
 
   return names.map((name: any) => ({
     slug: name.slug,
